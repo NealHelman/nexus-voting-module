ec82056a83acdf82b400737c8683469aa557f09c
NealHelman <nhelman@5thtradition.com>
2025-06-15 14:59:31 -0400

    Ensure we're in DEV when we expect to be in DEV

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,23 +1,8 @@
 // utils/env.js
 
-let USERNAME = null;
-let GENESIS = null;
-
-NEXUS.utilities.onWalletDataUpdated((walletData) => {
-  if (walletData?.userStatus?.username && walletData?.userStatus?.genesis) {
-    USERNAME = walletData.userStatus.username;
-    GENESIS = walletData.userStatus.genesis;
-  }
-});
-
 export const getVotingConfig = () => {
-  const isDev = window.location.pathname.includes('/repo/');
+  const isDev = process.env.NODE_ENV === 'development';
   const ENV = isDev ? 'DEV' : 'PROD';
   const VOTING_SIGCHAIN = ENV === 'DEV' ? 'VotingTestAuthority' : 'NexusCommunityVoting';
   return { ENV, VOTING_SIGCHAIN };
 };
-
-export const getWalletUserInfo = () => ({
-  username: USERNAME,
-  genesis: GENESIS,
-});
