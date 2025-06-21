49695a10ca1b48ad51a03c0d064192b308fd0fdb
NealHelman <nhelman@5thtradition.com>
2025-06-21 08:53:51 -0400

    Reinstate /src/App/index.js

---
+++ src/App/index.js
@@ -0,0 +1,14 @@
+import { useSelector } from 'react-redux';
+import { ModuleWrapper } from 'nexus-module';
+import Main from './Main';
+
+export default function App() {
+  const initialized = useSelector((state) => state.nexus.initialized);
+  const theme = useSelector((state) => state.nexus.theme);
+
+  return (
+    <ModuleWrapper initialized={initialized} theme={theme}>
+      {content}
+    </ModuleWrapper>
+  );
+}
